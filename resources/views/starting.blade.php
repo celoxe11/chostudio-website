a4280d2126d41c3b2152eae86c9aa4abd0bbfeae
Terta <tertania.a23@mhs.istts.ac.id>
2025-09-18 09:23:39 +0700

    halaman '/' wip

---
+++ resources/views/starting.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cho Studio</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/starting.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="contentbox flex flex-col items-center p-7 rounded-4xl outline outline-4 outline-black rounded-2xl shadow-[3vh_3vh_0_black]">
+    <div class="title">
+        <h1>
+            Cho's Studio
+        </h1>
+    </div>
+    <div class="button rounded-4xl outline outline-4 outline-black rounded-1xl shadow-[1vh_1vh_0_black]">
+        <a href="/login" class="go-button">Get Started</a>
+    </div>
+</div>
+
+</body>
+</html>
