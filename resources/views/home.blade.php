bbdeed6eaa8e49adbaa7beb2c438da5fbb6d9d7f
Terta <tertania.a23@mhs.istts.ac.id>
2025-10-03 01:36:35 +0700

    Fixed home page layout and background image
    kalau perlu background image buat halaman panggil aja "background.css"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,31 +11,53 @@
 
 <body class="flex flex-row items-center h-screen">
 
-    <!-- Sidebar -->
-    <div class="flex flex-col h-full bg-[#f0ebe3] w-[25%]">
-        <div class="title">Cho Studio</div>
-        <div class="link-socials">instagram, line</div>
+    {{-- sidebar --}}
+    <div class="flex flex-col items-center h-full bg-[#f0ebe3] w-[25%]">
+        <div class="personaImage bg-[#a2e1db] w-[30vh] h-[30vh] m-3">
+            OC Brenda
+        </div>
+        <div class="title w-[70%] h-fit m-3"><a href="/home"
+                class="flex items-center h-[12vh] p-2 bg-[#ffac81] rounded-2xl outline outline-3 outline-black 
+          shadow-[1vh_1vh_0_black]
+          flex justify-center 
+          font-[HammersmithOne-Regular] font-bold 
+          text-[#f0ebe3] text-[4vh]">
+                CHO.LAZEY
+            </a></div>
+        <div class="link-socials"><a href="http://">instagram</a>, <a href="http://">line</a></div>
 
-        <div class="button-group flex justify-center">
+        <div class="button-group flex justify-center font-[HammersmithOne-Regular]">
             <ul class="m-3">
-                <li class="m-3"> <button class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
+                <li class="m-3"> <button
+                        class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
                             href="http://">Gallery</a></button>
                 </li>
                 <li class="m-3">
-                    <button class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
+                    <button
+                        class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
                             href="http://">Commission</a></button>
                 </li>
                 <li class="m-3">
-                    <button class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
+                    <button
+                        class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
                             href="http://">Shop</a></button>
                 </li>
                 <li class="m-3">
-                    <button class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
+                    <button
+                        class="flex justify-center items-center bg-[#a2e1db] hover:bg-[#b4a6d5] p-3 rounded-2xl w-[30vh]"><a
                             href="http://">Work Progress</a></button>
                 </li>
             </ul>
         </div>
 
+    </div>
+
+    {{-- isi konten halaman --}}
+    <div class="content h-full w-full flex items-center justify-center bg-[url('/assets/images/Background.png')] bg-no-repeat bg-cover">
+        <div
+            class="flex flex-col justify-center items-center bg-[#f0ebe3] w-[80%] h-[80%] p-7 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
+            bababooey
+        </div>
     </div>
 
 </body>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,27 +7,26 @@
     <title>Cho Studio</title>
     <link rel="stylesheet" href="{{ asset('assets/css/background.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
+    {{-- WIP --}}
 </head>
 
 <body class="flex justify-center items-center h-screen">
-    <div class="flex flex-col justify-center items-center bg-[#f0ebe3] w-[75%] h-[70%] p-7 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
+    <div
+        class="flex flex-col justify-center items-center bg-[#f0ebe3] w-[75%] h-[70%] p-7 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
 
-        <!-- Title -->
+        {{-- judul --}}
         <div class="flex justify-center mb-6 font-bold font-[HammersmithOne-Regular] mt-[5vh]">
             <h1 class="text-2xl">CHO'S STUDIO</h1>
         </div>
-        
-            
+    </div>
 
-        </div>
-        
-        <!-- Login -->
-            <div class="loginbutton flex mt-[5vh] ml-[110vh] mr-[5vh] w-fit ">
-                <a href="/loginPage"
-                    class="flex justify-center text-2xl font-semibold text-[3vh] w-[20vh]  mt-[5vh]  outline rounded-[10vh] p-2 text-black hover:text-[#ffac81] transition">
-                    Login
-                </a>
-            </div>
+    {{-- tombol Login --}}
+    <div class="loginbutton flex mt-[5vh] ml-[110vh] mr-[5vh] w-fit ">
+        <a href="/loginPage"
+            class="flex justify-center text-2xl font-semibold text-[3vh] w-[20vh]  mt-[5vh]  outline rounded-[10vh] p-2 text-black hover:text-[#ffac81] transition">
+            Login
+        </a>
+    </div>
     </div>
 </body>
 
