446661e8b645f8e8decd0834a557079594e7a0d6
Terta <tertania.a23@mhs.istts.ac.id>
2025-10-02 14:36:48 +0700

    added Routes ("/home" and "/login")

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,35 +10,20 @@
 </head>
 
 <body class="flex justify-center items-center h-screen">
-    <div
-        class="flex flex-col justify-center items-center bg-[#f0ebe3] w-[75%] h-[70%] p-7 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
+    <div class="flex flex-col justify-center items-center bg-[#f0ebe3] w-[75%] h-[70%] p-7 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
 
         <!-- Title -->
         <div class="flex justify-center mb-6 font-bold font-[HammersmithOne-Regular] mt-[5vh]">
-            <h1 class="text-4xl md:text-6xl lg:text-8xl">CHO'S STUDIO</h1>
+            <h1 class="text-2xl">CHO'S STUDIO</h1>
         </div>
-
-        <!-- Buttons -->
-        <div class="flex flex-row gap-6 justify-center items-center">
-
-            <!-- Get Started -->
-            <a href="/home"
-   class="px-8 py-4 bg-[#ffac81] rounded-3xl outline outline-4 outline-black 
-          shadow-[1vh_1vh_0_black] 
-          transform transition-transform duration-300 
-          hover:scale-125 hover:rotate-[-5deg] 
-          flex justify-center items-center 
-          font-[HammersmithOne-Regular] font-bold 
-          text-[#f0ebe3] text-[5vh]">
-  Let’s Go
-</a>
+        
             
 
         </div>
         
         <!-- Login -->
             <div class="loginbutton flex mt-[5vh] ml-[110vh] mr-[5vh] w-fit ">
-                <a href="/login"
+                <a href="/loginPage"
                     class="flex justify-center text-2xl font-semibold text-[3vh] w-[20vh]  mt-[5vh]  outline rounded-[10vh] p-2 text-black hover:text-[#ffac81] transition">
                     Login
                 </a>
