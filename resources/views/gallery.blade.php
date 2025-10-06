322b293e8ab2abd16196a4684c454eef90b03bd3
Terta <tertania.a23@mhs.istts.ac.id>
2025-10-06 11:22:46 +0700

    Update gallery page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -12,9 +12,6 @@
             margin-left: 20vh;
             margin-top: 10vh;
         }
-        .buttons{
-            margin-right: 0;
-        }
         body {
             overflow: hidden;
         }
@@ -23,10 +20,10 @@
 
 <body class="h-screen">
 
-    <div class="navbar absolute  flex flex-row ml-[50%]">
-        <div class="flex justify-center items-center font-[HammersmithOne-Regular] bg-[#a2e1db] w-[20vh] h-[8.5vh] rounded-t-2xl outline border-black">
+        <div class="navbar absolute  flex flex-row ml-[50%] mt-[0.5vh]">
+        <button class="flex justify-center items-center font-[HammersmithOne-Regular] bg-[#a2e1db] w-[20vh] h-[8.5vh] rounded-t-2xl outline border-black hover:bg-[#b4a6d5] transition-colors duration-300 ease-in-out">
             Home
-        </div>
+        </button>
         <div class="flex justify-center items-center font-[HammersmithOne-Regular] bg-[#a2e1db] w-[20vh] h-[8.5vh] rounded-t-2xl outline border-black">
             Gallery
         </div>
@@ -54,6 +51,8 @@
                 class="folder flex justify-center flex-col bg-[#f0ebe3] w-[80%] h-[80%] mt-[-5%] p-10 rounded-3xl outline outline-4 outline-black shadow-[3vh_3vh_0_black]">
                 
             </div>
+    
+
     
 </body>
 
